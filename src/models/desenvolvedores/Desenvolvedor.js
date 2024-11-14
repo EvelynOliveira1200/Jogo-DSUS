f9241bdff94c849e66f7c2fc19d1b717e1df28e7
Evelyn <evelynoliveira.tds1.senai@gmail.com>
2024-11-14 11:50:36 -0300

    feat: update sabotador and desenvolvedor

--- src/models/desenvolvedores/Desenvolvedor.js
+++ src/models/desenvolvedores/Desenvolvedor.js
@@ -1,11 +1,8 @@
+
 class Desenvolvedor {
     constructor(aluno, equipe) {
-        this.id = this.generateId();
         this.aluno = aluno;
         this.equipe = equipe;
-    }
-    generateId() {
-        return Math.floor(Math.random() * 999) + 1;
     }
 }
 export default Desenvolvedor;
--- src/models/sabotadores/Sabotador.js
+++ src/models/sabotadores/Sabotador.js
@@ -1,11 +1,7 @@
 class Sabotador {
     constructor(aluno, equipe) {
-        this.id = this.generateId();
         this.aluno = aluno;
         this.equipe = equipe;
-    }
-    generateId() {
-        return Math.floor(Math.random() * 999) + 1;
     }
 }
 export default Sabotador;
